e4310cd7ee674e9cfac6de760332408f173c8aa1
Observer <80362048+68756d616e@users.noreply.github.com>
2022-10-22 00:26:44 +0100

    Add files via upload

---
+++ Age Calculator.py
@@ -0,0 +1,19 @@
+# Age calculator
+# If you were to live until 90, how long will you have remaining from your current age?
+
+#Input your current age
+age = input("What is your current age?")
+
+a = 90 - int(age)
+days = (a) * 365
+months = (a) * 12
+weeks = (a) * 52
+
+print(f"You have {days}, {weeks} weeks, and {months} months remaining")
+
+
+
+
+
+
+
